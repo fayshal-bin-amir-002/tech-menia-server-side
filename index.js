7cec71beae4a937a9ebafa31563992cf314610b5
fayshal69 <foyshalbinamir@gmail.com>
2024-08-14 23:43:34 +0600

    add: jwt token req and save a new user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +11,7 @@ app.use(express.json());
 
 const user = process.env.DB_USER;
 const pass = process.env.DB_PASS;
+const secret_access_token = process.env.SECRET_ACCESS_TOKEN;
 
 const uri = `mongodb+srv://${user}:${pass}@cluster0.0hiczfr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,11 +28,31 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
-        const database = client.db("gadgetsDB");
+        const database = client.db("techMeniaDB");
         const productsCollection = database.collection("products");
+        const usersCollection = database.collection("users");
+
+        //<---jwt token req--->
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, secret_access_token, { expiresIn: "1h" });
+            res.send({ token: token });
+        })
+
+        //<---save a user to db--->
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user?.email };
+            const isExist = await usersCollection.findOne(query);
+
+            if (isExist) return res.send({ message: "User already exists!" });
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
